1bc155099dc42b6ea972fb62247840e73c1b5491
gkpk30 <guykoomer@hotmail.com>
2022-05-10 10:14:08 -0700

    fixed header title position

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -8,10 +8,10 @@ import HeroImg from '../assets/images/eco-friendly-face-min.jpeg'
 
 export default function Header() {
   return (
-    <div className="mt-20 font-third">
-        <div className=" flex flex-col items-center text-[#444]">
-            <span className=" absolute top-[18%] text-xl">Beauty & Hair</span>
-            <span className="absolute top-[20%] text-8xl ">Blog</span>
+    <div className="mt-20 font-third relative ">
+        <div className=" flex flex-col items-center text-[#444] ">
+            <span className=" absolute top-5 text-xl">Beauty & Fashion</span>
+            <span className="absolute top-10 text-8xl ">Blog</span>
         </div>
         <img className="headerImg w-full h-5/6 mt-24 object-cover " src={HeroImg} alt="lady eco friendly" />
     </div>
