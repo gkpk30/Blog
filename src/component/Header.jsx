beeb2d20cedbf9e5145078f37dcfcfa579e9745a
gkpk30 <guykoomer@hotmail.com>
2022-05-11 09:38:54 -0700

    fixed header

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -9,8 +9,8 @@ import HeroImg from '../assets/images/eco-friendly-face-min.jpeg'
 export default function Header() {
   return (
     <div className="  mt-20 font-third  ">
-        <div className=" flex flex-col items-center text-[#444] h-14 relative bg-yellow-800  ">
-            <span className="  absolute top-[0%]  text-xl">Beauty & Fashion</span>
+        <div className=" flex flex-col items-center text-[#444] h-14 relative   ">
+            <span className="  absolute top-[0%]  text-xl  ">Beauty & Fashion</span>
             <span className="absolute top-[30%]    text-8xl ">Blog</span>
         </div>
         <img className=" w-full h-5/6  object-cover " src={HeroImg} alt="lady eco friendly" />
